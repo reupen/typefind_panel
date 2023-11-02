d96e8fcdf8abef9894dbbe484cf118d4c8788247
Reupen Shah <reupen@users.noreply.github.com>
2023-11-02 13:35:18 +0000

    Bring everything up to date
    This brings everything up to date and in line with other projects, including updating the foobar2000 SDK, adding pre-commit, migrating to GitHub Actions and updating to Visual Studio 2022.
    
    The licence was also changed to a more permissive one.

--- foo_uie_typefind/main.h
+++ foo_uie_typefind/main.h
@@ -1,13 +1,17 @@
 #pragma once
 
+#define NOMINMAX
 #define OEMRESOURCE
 #define SEARCH_CACHING_ENABLED
 
-#include "../foobar2000/SDK/foobar2000.h"
-#include "../foobar2000/helpers/helpers.h"
+// Included before windows.h, because pfc.h includes winsock2.h
+#include "../pfc/pfc.h"
 
-#include <commctrl.h>
+#include <Windows.h>
 #include <windowsx.h>
+#include <CommCtrl.h>
+
+#include "../foobar2000/SDK/foobar2000.h"
 
 #include "../columns_ui-sdk/ui_extension.h"
 #include "../ui_helpers/stdafx.h"
@@ -15,116 +19,106 @@
 #include "resource.h"
 #include "progressive_search.h"
 
+class quickfind_window : public uie::container_uie_window_v3 {
+    bool m_initialised;
+    WNDPROC m_editproc;
 
-class quickfind_window : public ui_extension::container_ui_extension {
-	bool m_initialised;
-	WNDPROC m_editproc;
+    bool m_is_running;
 
-	bool m_is_running;
+    progressive_search m_search;
 
-	progressive_search m_search;
+    unsigned height;
+    pfc::string8 m_pattern;
+    t_uint32 m_mode;
 
-	unsigned height;
-	pfc::string8 m_pattern;
-	t_uint32 m_mode;
-
-	modal_dialog_scope m_config_scope;
+    modal_dialog_scope m_config_scope;
 
 protected:
-	HWND wnd_edit;
-	HWND wnd_prev;
+    HWND wnd_edit;
+    HWND wnd_prev;
 
 public:
+    static pfc::ptr_list_t<quickfind_window> list_wnd;
+    LRESULT on_message(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
 
-	static pfc::ptr_list_t<quickfind_window> list_wnd;
-	LRESULT on_message(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
+    LRESULT WINAPI on_hook(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
+    static LRESULT WINAPI hook_proc(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
+    INT_PTR ConfigPopupProc(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
 
-	LRESULT WINAPI on_hook(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
-	static LRESULT WINAPI hook_proc(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
-	static BOOL CALLBACK ConfigPopupProc(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
+    quickfind_window();
 
-	quickfind_window();
+    static void g_update_all_fonts();
 
-	static void g_update_all_fonts();
+    ~quickfind_window();
 
-	~quickfind_window();
+    static const GUID extension_guid;
 
-	static const GUID extension_guid;
+    virtual const GUID& get_extension_guid() const { return extension_guid; }
 
-	virtual const GUID & get_extension_guid() const
-	{
-		return extension_guid;
-	}
+    virtual void get_name(pfc::string_base& out) const;
+    virtual void get_category(pfc::string_base& out) const;
 
+    enum {
+        stream_version_current = 0
+    };
+    void get_config(stream_writer* p_out, abort_callback& p_abort) const;
+    void set_config(stream_reader* p_source, t_size p_size, abort_callback& p_abort);
 
-	virtual void get_name(pfc::string_base & out)const;
-	virtual void get_category(pfc::string_base & out)const;
+    unsigned get_type() const { return ui_extension::type_panel; }
 
-	enum { stream_version_current = 0 };
-	void get_config(stream_writer * p_out, abort_callback & p_abort)const;
-	void set_config(stream_reader * p_source, t_size p_size, abort_callback & p_abort);
+    uie::container_window_v3_config get_window_config() override { return {L"{89A3759F-348A-4e3f-BF43-3D16BC059186}"}; }
 
-	unsigned get_type() const { return ui_extension::type_panel; }
+    static void update_all_window_frames();
 
-	virtual class_data & get_class_data()const
-	{
-		__implement_get_class_data(_T("{89A3759F-348A-4e3f-BF43-3D16BC059186}"), true);
-	}
+    void activate(bool b_focus = true)
+    {
+        // if (GetFocus() != wnd_edit)
+        {
+            height = uGetFontHeight(g_font) + 2;
+            if (b_focus) {
+                SetFocus(wnd_edit);
+            }
+            uSendMessage(wnd_edit, EM_SETSEL, 0, -1);
+            const auto text = uGetWindowText(wnd_edit);
+            m_search.init();
+            if (text.length())
+                m_search.set_string(text);
+            get_host()->on_size_limit_change(
+                get_wnd(), ui_extension::size_limit_minimum_height | ui_extension::size_limit_maximum_height);
+            on_size();
+        }
+    }
+    void on_size(unsigned cx, unsigned cy);
+    void on_size();
 
-	static void update_all_window_frames();
+    virtual bool have_config_popup() const { return true; }
+    virtual bool show_config_popup(HWND wnd_parent);
 
-	void activate(bool b_focus = true)
-	{
-		//if (GetFocus() != wnd_edit)
-		{
-			height = uGetFontHeight(g_font) + 2;
-			if (b_focus)
-			{
-				SetFocus(wnd_edit);
-			}
-			uSendMessage(wnd_edit, EM_SETSEL, 0, -1);
-			string_utf8_from_window text(wnd_edit);
-			m_search.init();
-			if (text.length()) m_search.set_string(text);
-			get_host()->on_size_limit_change(get_wnd(), ui_extension::size_limit_minimum_height | ui_extension::size_limit_maximum_height);
-			on_size();
-		}
-	}
-	void on_size(unsigned cx, unsigned cy);
-	void on_size();
+    class menu_node_activate : public ui_extension::menu_node_command_t {
+        service_ptr_t<quickfind_window> p_this;
 
-	virtual bool have_config_popup()const { return true; }
-	virtual bool show_config_popup(HWND wnd_parent);
+    public:
+        virtual bool get_display_data(pfc::string_base& p_out, unsigned& p_displayflags) const
+        {
+            p_out = "Activate";
+            p_displayflags = 0;
+            return true;
+        }
+        virtual bool get_description(pfc::string_base& p_out) const { return false; }
+        virtual void execute() { p_this->activate(); }
+        menu_node_activate(quickfind_window* host) : p_this(host){};
+    };
 
-	class menu_node_activate : public ui_extension::menu_node_command_t {
-		service_ptr_t<quickfind_window> p_this;
-	public:
-		virtual bool get_display_data(pfc::string_base & p_out, unsigned & p_displayflags) const
-		{
-			p_out = "Activate";
-			p_displayflags = 0;
-			return true;
-		}
-		virtual bool get_description(pfc::string_base & p_out) const
-		{
-			return false;
-		}
-		virtual void execute()
-		{
-			p_this->activate();
-		}
-		menu_node_activate(quickfind_window * host) : p_this(host) {};
-	};
+    virtual void get_menu_items(ui_extension::menu_hook_t& p_hook)
+    {
+        ui_extension::menu_node_ptr p_node(new uie::menu_node_configure(this));
+        ui_extension::menu_node_ptr p_node2(new menu_node_activate(this));
+        p_hook.add_node(p_node);
+        p_hook.add_node(p_node2);
+    }
 
-	virtual void get_menu_items(ui_extension::menu_hook_t & p_hook)
-	{
-		ui_extension::menu_node_ptr p_node(new uie::menu_node_configure(this));
-		ui_extension::menu_node_ptr p_node2(new menu_node_activate(this));
-		p_hook.add_node(p_node);
-		p_hook.add_node(p_node2);
-	}
+    friend class font_notify;
 
-	friend class font_notify;
 private:
-	static HFONT g_font;
+    static HFONT g_font;
 };
