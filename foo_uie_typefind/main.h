6eeac58a992d200fdff3d15f14278ffd76a82a90
Reupen Shah <reupen@users.noreply.github.com>
2025-09-19 22:23:45 +0100

    Add search beginning of words search mode
    This adds a new search mode that searches the beginning of any word in a formatted title.
    
    Both this mode and the existing mode to match the beginning of an entire formatted title now also ignore symbols and diacritics, and treat full-width and half-width characters as the same.

--- foo_uie_typefind/main.h
+++ foo_uie_typefind/main.h
@@ -1,26 +1,5 @@
 #pragma once
 
-#define NOMINMAX
-#define OEMRESOURCE
-#define SEARCH_CACHING_ENABLED
-
-#include <ranges>
-
-// Included before windows.h, because pfc.h includes winsock2.h
-#include "../pfc/pfc.h"
-
-#include <Windows.h>
-#include <windowsx.h>
-
-#include <wil/win32_helpers.h>
-
-#include "../foobar2000/SDK/foobar2000.h"
-
-#include "../columns_ui-sdk/ui_extension.h"
-#include "../ui_helpers/stdafx.h"
-#include "../fbh/stdafx.h"
-
-#include "resource.h"
 #include "progressive_search.h"
 
 using namespace uih::literals::spx;
@@ -95,10 +74,12 @@ public:
             SetFocus(m_wnd_edit);
         }
         SendMessage(m_wnd_edit, EM_SETSEL, 0, -1);
-        const auto text = uGetWindowText(m_wnd_edit);
+        const auto text = uih::get_window_text(m_wnd_edit);
         m_search.init();
-        if (text.length())
-            m_search.set_string(text);
+
+        if (!text.empty())
+            m_search.set_string(std::move(text));
+
         get_host()->on_size_limit_change(
             get_wnd(), ui_extension::size_limit_minimum_height | ui_extension::size_limit_maximum_height);
         on_size();
@@ -137,7 +118,7 @@ private:
     bool m_initialised{};
     bool m_is_running{};
     int height{};
-    t_uint32 m_mode{};
+    SearchMode m_mode{};
     ProgressiveSearch m_search;
     pfc::string8 m_pattern;
     std::unique_ptr<ColourNotifier> m_colours_notifier;
--- foo_uie_typefind/progressive_search.h
+++ foo_uie_typefind/progressive_search.h
@@ -1,13 +1,11 @@
-
 #pragma once
-
-#include <ranges>
 
 namespace typefind_panel {
 
-enum t_search_mode {
-    mode_pattern_beginning = 0,
+enum class SearchMode : uint32_t {
+    mode_match_beginning_formatted_title = 0,
     mode_query = 1,
+    mode_match_words_beginning_formatted_title = 2,
 };
 
 class ProgressiveSearch {
@@ -22,15 +20,16 @@ public:
             m_filter.set_size(count);
             m_filter.fill(true);
             m_buffer.prealloc(256);
-#ifdef SEARCH_CACHING_ENABLED
-            m_formatted.set_size(count);
-            if (m_mode == mode_pattern_beginning) {
+
+            if (m_mode == SearchMode::mode_match_beginning_formatted_title
+                || m_mode == SearchMode::mode_match_words_beginning_formatted_title) {
+                m_formatted.resize(count);
+
                 for (const auto n : std::ranges::views::iota(size_t{}, count)) {
                     m_entries[n]->format_title(nullptr, m_buffer, m_to, nullptr);
-                    m_formatted[n] = m_buffer;
+                    m_formatted[n] = mmh::to_utf16(m_buffer.c_str());
                 }
             }
-#endif
         }
         m_running = true;
     }
@@ -42,30 +41,28 @@ public:
         m_entries.remove_all();
         m_string.clear();
         m_buffer.clear();
-#ifdef SEARCH_CACHING_ENABLED
-        m_formatted.set_size(0);
-#endif
+        m_formatted.clear();
     }
 
     void add_char(unsigned c)
     {
         init();
-        m_string.add_char(c);
+        m_string.push_back(c);
         run();
     }
 
-    void set_string(const char* src)
+    void set_string(std::wstring text)
     {
         if (m_running)
             m_filter.fill(true);
         init();
-        m_string.set_string(src);
+        m_string = std::move(text);
         run();
     }
 
     void set_pattern(const char* src) { titleformat_compiler::get()->compile(m_to, src); }
 
-    void set_mode(t_uint32 mode) { m_mode = mode; }
+    void set_mode(SearchMode mode) { m_mode = mode; }
 
     bool on_key(WPARAM wp)
     {
@@ -126,85 +123,21 @@ public:
         }
         return false;
     }
-    ProgressiveSearch() : m_running(false), m_active(0), m_mode(mode_pattern_beginning) {}
+    ProgressiveSearch() : m_running(false), m_active(0), m_mode(SearchMode::mode_match_beginning_formatted_title) {}
 
 private:
-    void run()
-    {
-        bool b_clear = m_mode == mode_query;
-        if (m_mode == mode_query && m_string.get_length()) {
-            static_api_ptr_t<search_filter_manager_v2> api;
-            search_filter_v2::ptr p_filter;
-            try {
-                p_filter = api->create_ex(m_string, new service_impl_t<completion_notify_dummy>(),
-                    search_filter_manager_v2::KFlagSuppressNotify);
-                b_clear = false;
-            } catch (const pfc::exception&) {
-            }
-
-            if (p_filter.is_valid()) {
-                p_filter->test_multi(m_entries, m_filter.get_ptr());
-            }
-        }
-
-        if (!b_clear && m_string.get_length()) {
-            bool b_first = true;
-            size_t focus;
-            for (const auto n : std::ranges::views::iota(size_t{}, m_entries.get_count())) {
-                if (m_mode == mode_query) {
-                    if (m_filter[n]) {
-                        focus = n;
-                        b_first = false;
-                        break;
-                    }
-                } else {
-                    if (m_filter[n]) {
-#ifdef SEARCH_CACHING_ENABLED
-                        if (!stricmp_utf8_max(m_formatted[n], m_string, m_string.length()))
-#else
-                        m_entries[n]->format_title(0, m_buffer, m_to, 0);
-                        if (!stricmp_utf8_max(m_buffer, m_string, m_string.length()))
-#endif
-                        {
-                            if (b_first) {
-                                focus = n;
-                                b_first = false;
-                            }
-                        } else
-                            m_filter[n] = false;
-                    }
-                }
-            }
-
-            // api->playlist_set_selection(active, bit_array_true(), bit_array_table_t<bool>(m_filter.get_ptr(),
-            // m_filter.get_size()));
-            const size_t the_focus = m_playlist_api->playlist_get_focus_item(m_active);
-            if (!b_first && !(the_focus < m_filter.get_size() && m_filter[the_focus])) {
-                // api->playlist_set_focus_item(active, focus);
-                m_playlist_api->playlist_set_selection(m_active, bit_array_true(), bit_array_one(focus));
-                m_playlist_api->playlist_set_focus_item(m_active, focus);
-            } else if (!b_first && the_focus != pfc_infinite) {
-                m_playlist_api->playlist_set_selection(m_active, bit_array_true(), bit_array_one(the_focus));
-                m_playlist_api->playlist_ensure_visible(m_active, the_focus);
-                // api->playlist_ensure_visible(active, the_focus);
-            }
-        } else {
-            m_playlist_api->playlist_clear_selection(m_active);
-        }
-    }
+    void run();
 
     bool m_running{};
     metadb_handle_list_t<pfc::alloc_fast_aggressive> m_entries;
     service_ptr_t<titleformat_object> m_to;
     pfc::array_t<bool> m_filter;
-    pfc::string8_fastalloc m_string;
+    std::wstring m_string;
     pfc::string8_fastalloc m_buffer;
     static_api_ptr_t<playlist_manager> m_playlist_api;
     size_t m_active{};
-    t_uint32 m_mode{};
-#ifdef SEARCH_CACHING_ENABLED
-    pfc::array_t<pfc::string_simple> m_formatted;
-#endif
+    SearchMode m_mode{};
+    std::vector<std::wstring> m_formatted;
 };
 
 } // namespace typefind_panel
